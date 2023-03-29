d49827e11af47335195d98672bcbd142f6b59056
MauRodriguez <mrodriguezb@fi.uba.ar>
2023-03-29 16:57:04 -0300

    added client logic and new socket wrapper

--- client/common/client.py
+++ client/common/client.py
@@ -1,6 +1,11 @@
 import logging
 import time
 from cust_socket import Socket
+MAX_MSG_LENGHT = 8000
+BET_CODE = "B"
+RESULT_CODE = "R"
+WAIT_CODE = "W"
+OK_CODE = "O"
 
 class Client:
     def __init__(self, config_params):
@@ -20,18 +25,18 @@ class Client:
                     config_params["apuesta_numero"]])
 
     def run(self):
-        timeout = time.monotonic() + self._loop_lapse        
+        #timeout = time.monotonic() + self._loop_lapse        
 
-        while time.monotonic() < timeout and self._keep_running:
-            self._client_socket.connect()
+        #while time.monotonic() < timeout and self._keep_running:
+        self._client_socket.connect()
 
-            self._send_msg()
-            self._recv_msg()
-            
-            time.sleep(self._loop_period)
-            
-            self._current_msg_id += 1
-            self._client_socket.close()
+        self._send_msg(self._bet)
+        self._recv_msg(OK_CODE)
+        
+        # time.sleep(self._loop_period)
+        
+        self._current_msg_id += 1
+        self._client_socket.close()
     
     def stop(self):
         self._keep_running = False
@@ -39,12 +44,27 @@ class Client:
         logging.info(f"[CLIENT {self._client_id}] Gracefully closing client socket")
 
     def _send_msg(self, msg):
-        msg_length = str(len(msg.encode())).encode()
+        try:
+            msg_encoded = msg.encode()
+            msg_encoded_lenght = len(msg_encoded).to_bytes(6, "little", signed=False)
 
-        bytes_sent = self._client_socket.send_msg(msg_length)
-        logging.debug(f"action: send_msg | result: success | bytes_sent: {bytes_sent}")
+            if(len(msg_encoded) > MAX_MSG_LENGHT):
+                raise Exception("Message lenght to long")
+
+            self._client_socket.send_msg(BET_CODE.encode())
+            self._client_socket.send_msg(msg_encoded_lenght)
+            self._client_socket.send_msg(msg_encoded)
+            logging.debug(f"action: send_msg | result: success")
+        except Exception as e:
+            logging.error(f"action: send_msg | result: error | error: {e.args}")
+        
     
-    def _recv_msg(self):
-        received_msg = self._client_socket.recv_msg()            
+    def _recv_msg(self, SPECTED_CODE):
+        try:
+            received_msg = self._client_socket.recv_msg(len(SPECTED_CODE.encode()))
+            if(received_msg != SPECTED_CODE):
+                raise Exception("Message received not match with spected")
+        except Exception as e:
+            logging.error(f"action: recv_msg | result: error | error: {e.args}")            
         logging.debug(f"action: recv_msg | result: success | bytes_recv: {len(received_msg.encode())}")
         return received_msg
--- client/common/cust_socket.py
+++ client/common/cust_socket.py
@@ -10,26 +10,30 @@ class Socket:
         try:
             self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self._socket.connect(self._address)
-            logging.debug(f"action: connect | result: success | error: {e}")
+            logging.debug(f"action: connect | result: success | error: {repr(e)}")
         except socket.timeout as e:
-            logging.error(f"action: connect | result: error | error: {e}")
+            logging.error(f"action: connect | result: error | error: {repr(e)}")
         except socket.error as e:
-            logging.error(f"action: connect | result: error | error: {e}")
+            logging.error(f"action: connect | result: error | error: {repr(e)}")
 
     def close(self):
         self._socket.close()
 
-    def recv_msg(self):
+    def recv_msg(self, lenght):
         try:
-            received_msg = self._socket.recv(1024).decode()
+            received_msg = self._socket.recv(lenght)
+            while len(received_msg) < lenght:
+                received_msg.append(self._socket.recv(lenght - len(received_msg)))
+
             logging.debug(f"action: recv | result: success | msg: {received_msg}")
-            return received_msg
+
+            return received_msg.decode()
         except socket.error as e:
-            logging.error(f"action: recv | result: error | error: {e}") 
+            logging.error(f"action: recv | result: error | error: {repr(e)}") 
         
     def send_msg(self, msg):
         try:
             self._socket.sendall(msg.encode())
             logging.debug(f"action: sendall | result: success | msg: {msg}")
         except socket.error as e:
-            logging.error(f"action: sendall | result: error | error: {e}")
+            logging.error(f"action: sendall | result: error | error: {repr(e)}")
